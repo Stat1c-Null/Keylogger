11345a36caefeb3ed1f2e810ba409ee16fb283be
Mikita Slabysh <slabysh2015@gmail.com>
2022-03-01 13:12:10 -0500

    Added C++ exe launcher

---
+++ Keylogger/mail_sending/LaunchExe/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    cout << "Launching exe" << endl;
+    system("explorer D:\\Programming\\C++\\Keylogger\\mail_sending\\main.exe ");
+    system("pause");
+    return 0;
+}
---
+++ Keylogger/mail_sending/launch_python.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
